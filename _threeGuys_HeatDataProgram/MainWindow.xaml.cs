1d475b3d3e22157764df7f462d0685bb69a8b75b
namwlee99 <sideoftrump@pm.me>
2023-11-28 14:46:24 +0900

    Add SocketSystem

--- _threeGuys_HeatDataProgram/MainWindow.xaml.cs
+++ _threeGuys_HeatDataProgram/MainWindow.xaml.cs
@@ -9,16 +9,47 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using ThreadRunner;
+using PySocketHandler;
+
 namespace _threeGuys_HeatDataProgram
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        PySocketHandler.PySocketHandler psh = new PySocketHandler.PySocketHandler();
+        ThreadRunner.ThreadRunner thr = new ThreadRunner.ThreadRunner();
+
         public MainWindow()
         {
             InitializeComponent();
+            psh.prepareSocket();
         }
+
+        /*
+        private async void button_tempSocket_Click(object sender, RoutedEventArgs e)
+        {
+            string sendString = textBox_tempSocketSend.Text;
+
+            // 비동기로 데이터 보내기
+            await Task.Run(() =>
+            {
+                psh.sendSocketString(sendString);
+            });
+
+            textBox_tempSocketSend.Text = "";
+
+            // 비동기로 데이터 받기
+            Task<string> receiveTask = Task.Run(() => psh.receivedSocketString());
+
+            // UI 스레드 차단하지 않고 받은 데이터를 기다림
+            while (!receiveTask.IsCompleted)
+            {
+                await Task.Delay(1); // 100ms 마다 체크
+            }
+
+            textBox_tempSocketReceived.Text = receiveTask.Result;
+        }
+        */
+
     }
 }
---
+++ _threeGuys_HeatDataProgram/PySocketHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PySocketHandler
+{
+    class PySocketHandler
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+
+        public string prepareSocket()
+        {
+            try
+            {
+                string serverIp = "127.0.0.1";
+                int serverPort = 56789;
+
+                client = new TcpClient(serverIp, serverPort);
+
+                stream = client.GetStream();
+
+                return "Success";
+            }
+            catch (Exception e)
+            {
+                return ("Error: " + e.Message);
+            }
+        }
+
+        public void sendSocketString(string sendString)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(sendString);
+            stream.Write(data, 0, data.Length);
+        }
+
+        public string receivedSocketString()
+        {
+            byte[] data = new byte[256];
+            int bytesRead = stream.Read(data, 0, data.Length);
+            string receivedMessage = Encoding.UTF8.GetString(data, 0, bytesRead);
+
+            return receivedMessage;
+        }
+
+        public string checkReceivedString(string checkString)
+        {
+            string[] strings = checkString.Split( );
+            if (strings[0] == "1")
+            {
+                return "1";
+            }
+
+            else if (strings[0] == "2")
+            {
+                return "2";
+            }
+
+            return "Error: " + strings[0] + "is not define";
+        }
+    }
+}
